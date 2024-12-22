454cc952314bc74352a5e38c4b99808d1a0391ba
Joakim Tosteberg <joakim@tosteberg.se>
2024-12-22 11:31:59 +0100

    day22: Slight optimizations

--- 22/day22.py
+++ 22/day22.py
@@ -14,30 +14,47 @@ def get_next_secret(secret):
     return secret
 
 
-sequence_bananas = {}
 secret_sum = 0
+lookup = {}
+for i in range(-9,10):
+    lookup[i] = dict()
+    for j in range(-9,10):
+        lookup[i][j] = dict()
+        for k in range(-9,10):
+            lookup[i][j][k] = dict()
+            for l in range(-9,10):
+                lookup[i][j][k][l] = 0
+
+max_bananas = 0
 for secret in secrets:
     last = secret % 10
     local_lookup = set()
-    seq_queue = collections.deque(maxlen=4)
-    for i in range(0,2000):
+    seq = (0,0,0,0)
+    for i in range(0,3):
         secret = get_next_secret(secret)
         price = secret % 10
         change = price - last
         last = price
 
-        seq_queue.appendleft(change)
-        if i < 3:
-            continue
-        seq = tuple(seq_queue)
+        seq = (seq[1], seq[2], seq[3], change)
+    for i in range(3,2000):
+        secret = get_next_secret(secret)
+        price = secret % 10
+        change = price - last
+        last = price
+
+        seq = (seq[1], seq[2], seq[3], change)
         if seq not in local_lookup:
             local_lookup.add(seq)
-            if seq not in sequence_bananas:
-                sequence_bananas[seq] = price
-            else:
-                sequence_bananas[seq] += price
+            lookup[seq[0]][seq[1]][seq[2]][seq[3]] += price
 
     secret_sum += secret
 
 print(f"part1: {secret_sum}")
-print(f"part2: {max(sequence_bananas.values())}")
+max_bananas = 0
+for i in lookup.values():
+    for j in i.values():
+        for k in j.values():
+            for l in k.values():
+               max_bananas = max(max_bananas, l) 
+print(f"part2: {max_bananas}")
